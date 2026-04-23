22fe5e0b477c29eadef864161e1551afb85035b8
Guilherme <lerichlesss@gmail.com>
2026-04-23 18:52:38 -0300

    fix: habilita CORS

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const clientesRoutes = require('./src/routes/clientesRoutes');
 const authRoutes = require('./src/routes/authRoutes');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 
 // Middleware para ler JSON no corpo das requisições
 app.use(express.json());
